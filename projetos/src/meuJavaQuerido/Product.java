3bd8eabb5b24dffcb8e1e3b9a3705c9df1442999
Vitor Coelho <vijcoelho@gmail.com>
2024-02-23 22:40:35 -0300

    up

---
+++ projetos/src/meuJavaQuerido/Product.java
@@ -0,0 +1,44 @@
+package meuJavaQuerido;
+
+import java.util.Objects;
+
+public class Product {
+    private int id;
+    private String name;
+    private double price;
+
+    public Product(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean equals(Product other) {
+        return (this.id == other.getId() && this.name == other.getName());
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
