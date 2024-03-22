a72f28e377bfab9c1e4f0207b5b0c594a0371e8f
Vitor Coelho <vijcoelho@gmail.com>
2024-03-22 15:43:11 -0300

    up

--- leetCode/src/AddTwoNumbers.java
+++ leetCode/src/AddTwoNumbers.java
@@ -1,13 +1,30 @@
+import java.util.Arrays;
+
 public class AddTwoNumbers {
-    public void addTwoNumbers(int[] l1, int[] l2) {
-        for (int i = 0; i < l1.length; i++) {
-            l1[i] += l2[i];
-            System.out.println(l1[i]);
+    public int[] addTwoNumbers(int[] l1, int[] l2) {
+        StringBuilder sb = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+
+        for (int num: l1) {
+            sb.append(num);
         }
+        for (int num: l2) {
+            sb2.append(num);
+        }
+        int finalValor = Integer.parseInt(sb.toString()) + Integer.parseInt(sb2.toString());
+
+        String resultadoString = String.valueOf(finalValor);
+        int[] finalArray = new int[resultadoString.length()];
+
+        for (int i = 0; i < resultadoString.length(); i++) {
+            finalArray[resultadoString.length() - 1 - i] = Character.getNumericValue(resultadoString.charAt(i));
+        }
+
+        return finalArray;
     }
 
     public static void main(String[] args) {
         AddTwoNumbers add = new AddTwoNumbers();
-        add.addTwoNumbers(new int[]{2,4,3}, new int[]{5,6,4});
+        System.out.println(Arrays.toString(add.addTwoNumbers(new int[]{2, 4, 3}, new int[]{5, 6, 4})));
     }
 }
