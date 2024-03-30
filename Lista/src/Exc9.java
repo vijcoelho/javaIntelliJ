d125e61727db1cb05653ee65dafb31877711e284
Vitor Coelho <vijcoelho@gmail.com>
2024-03-29 23:58:28 -0300

    up

--- Lista/src/Exc9.java
+++ Lista/src/Exc9.java
@@ -1,17 +1,22 @@
 public class Exc9 {
     public static void apareceuN(int[] array) {
-        int vezes = 0;
-        for (int i = 0; i < array.length; i++) {
-            for (int j = i; j < array.length; j++) {
-                if(array[i] == array[j]) {
-                    vezes++;
-                }
+        int quantidadeIgual = 0;
+        int i = 0;
+
+        while (i < array.length) {
+            int j = i + 1;
+            while (j < array.length && array[i] == array[j]) {
+                j++;
             }
+            quantidadeIgual++;
+            i = j;
         }
-        System.out.println(vezes);
+
+        System.out.println(quantidadeIgual);
     }
 
     public static void main(String[] args) {
-        apareceuN(new int[]{1,2,3,4,5});
+        apareceuN(new int[]{1, 2, 3, 3, 3, 3, 3, 3, 4, 5});
     }
 }
+
